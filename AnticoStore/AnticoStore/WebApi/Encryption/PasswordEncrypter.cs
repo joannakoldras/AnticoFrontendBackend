92a7879d8feff8f4173f16855aedc344e7a9f4c8
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-11-21 21:17:02 +0100

    add PasswordEncrypter with implementation

---
+++ AnticoStore/AnticoStore/WebApi/Encryption/PasswordEncrypter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Encryption
+{
+    public class PasswordEncrypter
+    {
+        private string salt { get; set; } = "Alamakota";
+        private const int hashSize = 16; 
+        public string Encrypt(string password) 
+        {
+            var saltBytes = Encoding.ASCII.GetBytes(salt);
+            var hashAlgorithm = new Rfc2898DeriveBytes(password, saltBytes);
+            var hashCode = hashAlgorithm.GetBytes(hashSize);
+            var hashCodeString = Encoding.ASCII.GetString(hashCode);
+
+            return hashCodeString; 
+        }
+    }
+}
