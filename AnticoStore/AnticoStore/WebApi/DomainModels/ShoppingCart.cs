3843317a43d69032c808e00dbcb95e4a505359e3
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2023-11-21 19:56:28 +0100

    Add SessionHelper class and other components for session

--- AnticoStore/AnticoStore/WebApi/DomainModels/ShoppingCart.cs
+++ AnticoStore/AnticoStore/WebApi/DomainModels/ShoppingCart.cs
@@ -4,31 +4,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApi.Session;
 
 namespace WebApi.DomainModels
 {
     public class ShoppingCart
     {
         private List<Product> products { get; set; }
+        private SessionHelper sessionHelper { get; set; }
         public ShoppingCart()
         {
             this.products = new List<Product>();
+            this.sessionHelper = new SessionHelper();
         }
 
         public void AddToShoppingCart(Product product)
         {
-            products.Add(product);
+            sessionHelper.SetSessionWithValue(product);
         }
 
         public bool DeleteFromShoppingCart(Product product)
         {
-            var item = FindProductInShoppingCartById(product);
-            if (item != null)
-            {
-                this.products.Remove(item);
-                return true;
-            }
-            return false;
+            var result = sessionHelper.DeleteItemFromSession(product);
+            return result;
         }
 
         public Product FindProductInShoppingCartById(Product product)
@@ -39,7 +37,7 @@ namespace WebApi.DomainModels
 
         public IEnumerable<Product> GetAllProducts()
         {
-            return products;
+            return sessionHelper.GetAllItemsFromSession();
         }
     }
 }
---
+++ AnticoStore/AnticoStore/WebApi/Session/SessionHelper.cs
@@ -0,0 +1,87 @@
+﻿using Database.DbModels;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web.Mvc;
+
+namespace WebApi.Session
+{
+     public class SessionHelper 
+    {
+        private IHttpContextAccessor httpContextAccessor;
+        private int sessionItemCounter; 
+        public SessionHelper()
+        {
+            httpContextAccessor = new HttpContextAccessor();
+            sessionItemCounter = 0;
+            CountItemsInSession(); 
+        }
+        public void SetSessionWithValue(object value)
+        {
+            SetObjectAsJson("1", value); 
+        }
+
+        public IEnumerable<Product> GetAllItemsFromSession()
+        {
+            var items = new List<Product>(); 
+            for (int i=0; i<sessionItemCounter; i++)
+            {
+               // var sessionItem = GetObjectFromJson<Product>(i.ToString()); 
+                //items.Add(sessionItem); 
+            } 
+            return items;  
+        }
+
+        private void SetObjectAsJson(string key, object value)
+        {
+            //httpContextAccessor.HttpContext.Session.SetString(sessionItemCounter.ToString(), JsonConvert.SerializeObject(value));
+        }
+
+        //private T GetObjectFromJson<T>(string key)
+        //{
+        //    //var value = httpContextAccessor.HttpContext.Session.GetString(key);
+        //    //return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value); 
+        //    return string.Empty; 
+        //}
+
+        //do naprawy ponowne ustawianie obiektów w sesji
+        public bool DeleteItemFromSession(Product product)
+        {
+            var items = GetAllItemsFromSession().ToList();
+            var itemToDelete = items.Where(x => x.Id == product.Id).FirstOrDefault(); 
+
+            if(itemToDelete != null)
+            {
+                items.Remove(itemToDelete); 
+            }
+            else
+            {
+                return false; 
+            }
+
+            ClearSession(); 
+            var counter = 0; 
+            
+            foreach (var item in items)
+            {
+                SetObjectAsJson(counter.ToString(), item); 
+                counter++; 
+            }
+            return true; 
+        }
+
+        private void ClearSession()
+        {
+            httpContextAccessor.HttpContext.Session.Clear(); 
+        }
+
+        private void CountItemsInSession()
+        {
+            sessionItemCounter = httpContextAccessor.HttpContext.Session.Keys.Count(); 
+        }
+
+
+    }
+}
